bf4bf23d9adba25a126c973ab9420bd3d9e9a8c7
Nitzan Moise <nitz0002@stud.kea.dk>
2020-09-02 17:11:14 +0200

    second commit

---
+++ typewriter.js
@@ -0,0 +1,29 @@
+// "use strict";
+
+window.addEventListener("load", init);
+
+function init() {
+  const typeWriter = document.querySelector("#typewriter");
+  const typeWriterStr = typeWriter.textContent;
+  typeWriter.textContent = "";
+  let i;
+  let prevText = "";
+  const sound = document.getElementById("typekey1");
+  let audio = new Audio("typekey1.mp3");
+  audio.play();
+
+  for (i = 0; i < typeWriterStr.length; i++) {
+    let currChar = typeWriterStr.charAt(i);
+    addChar(currChar, prevText, typeWriter, i);
+  }
+
+  console.log(typeWriterStr);
+}
+
+function addChar(currChar, prevText, typeWriter, i) {
+  setTimeout(() => {
+    console.log(i);
+    prevText = typeWriter.textContent;
+    typeWriter.textContent = prevText + currChar;
+  }, i * 200);
+}
